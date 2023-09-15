7d0dcc30273e6af814e8c30fd45b14dcba97cf93
Rodolfo-ss <rodolfcs@gmai.com>
2023-09-15 06:22:57 -0300

    jump_commit

---
+++ jump_search.rb
@@ -0,0 +1,14 @@
+array = [1, 2, 4, 5, 44, 81, 92,65,43, 77, 55]
+
+
+def jump_search(target, form)
+    counter = 0
+    while counter < array.length
+        if array.counter == target
+            return counter
+        else 
+            counter += 1
+        end 
+    end
+    return nil
+end
